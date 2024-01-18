da8a2ea69c9ea58d02d40baf06e2f12da9b900fa
Renan Borges <renanvcb@gmail.com>
2024-01-18 19:25:46 -0300

    Initial commit with previous code

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Bem vindo ao jogo do número secreto!");
+let numeroSecreto = 5;
+let palpite = prompt("Entre com um número entre 1 e 10");
+
+if (palpite == numeroSecreto) {
+  alert("Você acertou!");
+} else {
+  alert("Você errou! :(");
+}
