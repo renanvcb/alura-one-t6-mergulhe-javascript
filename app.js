b481607a426e384da0c24d1bd9a44aa07e7f9259
renanvcb <renanvcb@gmail.com>
2024-01-19 00:11:27 -0300

    success text updated by the number of tries

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ while (palpite != numeroSecreto) {
   palpite = prompt("Entre com um número entre 1 e 10");
 
   if (palpite == numeroSecreto) {
-    alert(
-      `Parabéns! Você acerto o número secreto ${numeroSecreto} com ${tentativas} tentativas!`
-    );
+    break;
   } else {
     if (palpite > numeroSecreto) {
       alert(`O número secreto é menor que ${palpite}`);
@@ -19,4 +17,14 @@ while (palpite != numeroSecreto) {
     }
     tentativas++;
   }
+}
+
+if (tentativas > 1) {
+  alert(
+    `Parabéns! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas!`
+  );
+} else {
+  alert(
+    `Que sorte! Você acertou o número secreto ${numeroSecreto} na primeira tentativa!`
+  );
 }
