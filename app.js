3e69c7421f3c8f97fe984dfa6182b82550ef56d4
renanvcb <renanvcb@gmail.com>
2024-01-18 22:24:48 -0300

    added while and tries counter

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 alert("Bem vindo ao jogo do número secreto!");
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let palpite = prompt("Entre com um número entre 1 e 10");
+let palpite;
+let tentativas = 1;
 
-if (palpite == numeroSecreto) {
-  alert(`Parabéns! Você acerto o número secreto! (${numeroSecreto})`);
-} else {
-  if (palpite > numeroSecreto) {
-    alert(`O número secreto é menor que ${palpite}`);
+while (palpite != numeroSecreto) {
+  palpite = prompt("Entre com um número entre 1 e 10");
+
+  if (palpite == numeroSecreto) {
+    alert(
+      `Parabéns! Você acerto o número secreto ${numeroSecreto} com ${tentativas} tentativas!`
+    );
   } else {
-    alert(`O número secreto é maior que ${palpite}`);
+    if (palpite > numeroSecreto) {
+      alert(`O número secreto é menor que ${palpite}`);
+    } else {
+      alert(`O número secreto é maior que ${palpite}`);
+    }
+    tentativas++;
   }
 }
